fdd4c0522cbc7d7dea0002bc87a806bc0deb4494
rafael <rafaondjango@gmail.com>
2024-02-15 17:12:46 -0300

    feat: estilizando botões de agrupamento

--- components/dashboard/resources/grouping_options_filter.js
+++ components/dashboard/resources/grouping_options_filter.js
@@ -5,12 +5,24 @@ import { configChart, updateChart } from "/components/dashboard/resources/graphi
 export const configOptionButtonsAction = (invoicesData) => {
     configChart(invoicesData)
     $(".option_monthly").on("click", () => {
+        $(".option_annually").removeClass("active")
+        $(".option_semiannually").removeClass("active")
+
+        $(".option_monthly").addClass("active")
         updateChart(invoicesData, groupingOptions.MONTHLY)
     })
     $(".option_annually").on("click", () => {
+        $(".option_monthly").removeClass("active")
+        $(".option_semiannually").removeClass("active")
+
+        $(".option_annually").addClass("active")
         updateChart(invoicesData, groupingOptions.ANNUALLY)
     })
     $(".option_semiannually").on("click", () => {
+        $(".option_monthly").removeClass("active")
+        $(".option_annually").removeClass("active")
+
+        $(".option_semiannually").addClass("active")
         updateChart(invoicesData, groupingOptions.SEMIANNUALLY)
     })
 }
