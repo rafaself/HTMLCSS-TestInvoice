ecb42d5541389ac6f628ebbb9c7042448941eabf
rafael <rafaondjango@gmail.com>
2024-02-16 18:11:25 -0300

    feat: esperar dom carregar para tentar criar o gráfico

--- components/dashboard/resources/main.js
+++ components/dashboard/resources/main.js
@@ -2,8 +2,9 @@ import { populateCards } from "/components/dashboard/resources/cards_data.js"
 import { setupChartsContext } from "/components/dashboard/resources/charts_setup.js"
 
 export const main = async () => {
-    const invoicesData = await $.ajax("https://rafaself.github.io/data.json")
-    console.log(invoicesData)
-    setupChartsContext(invoicesData)
-    populateCards(invoicesData)
+    $().ready(async () => {
+        const invoicesData = await $.ajax("https://rafaself.github.io/data.json")
+        setupChartsContext(invoicesData)
+        populateCards(invoicesData)
+    })
 }
