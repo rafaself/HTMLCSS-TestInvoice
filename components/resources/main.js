eccb1b42141b4568b9b19883742cdbe7fa302e2a
rafael <rafaondjango@gmail.com>
2024-02-16 17:05:28 -0300

    fix: apgando arquivos nao utilizados

--- components/resources/main.js
+++ components/resources/main.js
@@ -3,7 +3,6 @@ import { configChartsContext } from "/components/resources/grouping_options_filt
 
 export const main = async () => {
     const invoicesData = await $.ajax("https://rafaself.github.io/data.json")
-    console.log("passa")
     configChartsContext(invoicesData)
     populateCards(invoicesData)
 }
