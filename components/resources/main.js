754a72aa3054c1f04bde8ec4f5fe8f2e44d8f053
rafael <rafaondjango@gmail.com>
2024-02-16 07:32:58 -0300

    feat: ajustando populacao de dados de acordo com mudanca de rotas

--- components/resources/main.js
+++ components/resources/main.js
@@ -2,8 +2,7 @@ import { invoicesData } from "/components/resources/generate_data.js";
 import { populateCards } from "/components/resources/cards_data.js"
 import { configOptionButtonsAction } from "/components/resources/grouping_options_filter.js"
 
-$(() => {
+export const main = () => {
     configOptionButtonsAction(invoicesData)
     populateCards(invoicesData)
-});
-
+}
