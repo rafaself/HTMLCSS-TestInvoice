e12c518990a04ca51268eaa68bde62a919b81bb9
rafael <rafaondjango@gmail.com>
2024-02-16 10:45:57 -0300

    feat: retiranod console.log

--- views/side-nav/handle_side_nav_styles.js
+++ views/side-nav/handle_side_nav_styles.js
@@ -1,6 +1,5 @@
 export const handleNavSideStyles = (path) => {
     $('.side-nav__buttons__button').each(function () {
-        console.log("passa no side tbm")
         $(this).removeClass("active")
         var href = $(this).find("a").attr("href")
 
